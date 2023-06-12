ec1cb794d2cb25b05f3f4be4ce5ac7e8b5d3323b
guilherme1712 <gdaudt17@gmail.com>
2023-06-11 22:59:35 -0300

    codigo main comentado

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,13 +6,21 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        // Criando uma instância do ScannerLeitor
         ScannerLeitor scanner = new ScannerLeitor();
+
+        // Caminho do arquivo CSV
         String fileName = "D:\\area de trabalho\\PUCPR\\3º SEMESTRE 2023\\1 - Programação Orientada a Objetos\\PJBL\\GIT\\POO-PjBL\\src\\main\\funcionarios.csv";
+
+        // Lendo o arquivo e obtendo a tabela de dados
         List<List<String>> tabela = scanner.leituraDoArquivo(fileName);
 
+        // Criando um mapa para armazenar os departamentos
         Map<String, Departamento> departamentos = new HashMap<>();
 
+        // Iterando sobre os registros da tabela
         for (List<String> registro : tabela) {
+            // Obtendo os dados do registro
             String cargo = registro.get(0);
             String nome = registro.get(1);
             double salario = Double.parseDouble(registro.get(2));
@@ -22,16 +30,20 @@ public class Main {
             double valorVendas = Double.parseDouble(registro.get(6));
             String numVendas = registro.get(7).trim();
 
+            // Convertendo o número de vendas para inteiro
             int newNumVendas = Integer.parseInt(numVendas);
 
+            // Verificando se o departamento já existe no mapa
             Departamento departamento;
             if (departamentos.containsKey(departamentoNome)) {
                 departamento = departamentos.get(departamentoNome);
             } else {
+                // Criando um novo departamento se não existir
                 departamento = new Departamento(departamentoNome);
                 departamentos.put(departamentoNome, departamento);
             }
 
+            // Criando o funcionário com base no cargo
             Funcionario funcionario;
             if (cargo.equals("Vendedor")) {
                 funcionario = new Vendedor(nome, salario, dataNascimento, departamento, valorVendas, newNumVendas);
@@ -39,18 +51,24 @@ public class Main {
                 funcionario = new Gerente(nome, salario, dataNascimento, departamento, bonus);
             }
 
+            // Adicionando o funcionário ao departamento
             departamento.adicionarFuncionario(funcionario);
         }
 
+        // Executando a interface gráfica em uma thread separada
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
+                // Criando uma lista de folhas de pagamento
                 List<FolhaPagamento> folhas = new ArrayList<>();
+
+                // Calculando a folha de pagamento para cada departamento
                 for (Departamento departamento : departamentos.values()) {
                     FolhaPagamento folha = new FolhaPagamento(Mes.JANEIRO, departamento);
                     folha.calcularTotalSalarios();
                     folhas.add(folha);
                 }
 
+                // Criando a interface gráfica e exibindo-a
                 InterfaceGrafica interfaceGrafica = new InterfaceGrafica(new ArrayList<>(departamentos.values()), (ArrayList<FolhaPagamento>) folhas);
                 interfaceGrafica.setVisible(true);
             }
